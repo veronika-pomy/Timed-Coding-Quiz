71780fbc511642df26321cc946dc1b293ab68ae4
VVPCodes <pomyateevav@gmail.com>
2022-12-11 20:04:29 -0500

    added basic styling in css, added a div for timer and button and a div for the question in html, added eventlistener to button to start timer in js

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # References
-Google API for font
+- Google API for font
+- setInterval docs
+- how to add setInterval inside an eventListener
+https://stackoverflow.com/questions/17614067/addeventlistener-and-setinterval-fail-silently-in-a-greasemonkey-script
+- Fix for the interval going faster when btn is clicked twice
+https://stackoverflow.com/questions/42794107/why-does-my-interval-go-faster-the-second-time-i-execute-it-jquery
--- assets/index.js
+++ assets/index.js
@@ -1,18 +1,20 @@
-let startSecondsEl = 60; // start timer at 60 seconds 
-const countDownEl = document.querySelector("#count-down"); // targeting html tag displaying countdown
-
-// function to update the counter 
-function countDownSeconds () {
-    if (startSecondsEl >= 0) {
-        let secondsRemains = startSecondsEl;
-        startSecondsEl--; // decreasing time 
-        countDownEl.textContent = `${secondsRemains} seconds remains`;
-    } else {
-        countDownEl.textContent = `Time's up!`;
-    }
-}
-
-// calling the function to update counter every millisecond
-setInterval(countDownSeconds, 1000); 
-
-// next put in an event listener to start when the button start is clicked
+// start timer at 60 seconds 
+let startSecondsEl = 60;
+// selecting html tag displaying countdown
+const countDownEl = document.querySelector("#count-down"); 
+// selecting button el
+const buttonEl = document.querySelector("#start-button"); 
+// Event listener to start countdown when the button "start" is clicked
+// Includes set interval method and a funcion that increments seconds
+// Fix that when the button is clicked twice the timer goes faster
+buttonEl.addEventListener("click", function () {
+    setInterval ( function (){
+        if (startSecondsEl >= 0) {
+            let secondsRemains = startSecondsEl;
+            startSecondsEl--; // decreasing time 
+            countDownEl.textContent = `${secondsRemains} seconds remains`;
+        } else {
+            countDownEl.textContent = `Time's up!`;
+        }
+    },1000);
+});
